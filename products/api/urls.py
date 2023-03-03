85e78dda01e44d2d02bb81d4d2fce46625121f69
omersb <omersaidbuldukk@gmail.com>
2023-03-03 11:09:27 +0300

    views added

--- products/api/urls.py
+++ products/api/urls.py
@@ -1,8 +1,15 @@
-from .views import CategoriesViews
+from .views import (
+    CategoriesViews,
+    ProductsViews,
+    LikeViews,
+    CommentsViews)
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register("category", CategoriesViews, basename="category")
+router.register("products", ProductsViews, basename="products")
+router.register("like", LikeViews, basename="like")
+router.register("comments", CommentsViews, basename="comments")
 
 
 urlpatterns = [
